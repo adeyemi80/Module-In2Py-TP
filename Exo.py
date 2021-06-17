b78da7db8c0e62b7e131c80356493dd38d113713
adeyemi80 <85198512+adeyemi80@users.noreply.github.com>
2021-06-17 16:19:28 +0100

    Exo

---
+++ Exo.py
@@ -0,0 +1,19 @@
+#Exo 1
+
+kolawole = {analyse, eqtdif, langageC, java, reseau, SE, uml, sad }
+valeurs = { 'analyse':[13, 14,15], 'eqtdif':[15,15,17],'langage': [11,12,15], 'java':[16,13,14], 'reseau':[18,15,14], 'SE':[10,11,14], 'uml':[11,14,13],'sad':[15,16,13]}
+
+print(kolawole(moyenne))
+
+#Exo 2
+
+L ={1,2,3,4,5,6,7,8,9,10}
+print(list(L))
+    
+
+#Exo 3
+
+d={'nom':'depuis','prenom':'Jacque','age':30}
+
+d.values()
+d.keys()
